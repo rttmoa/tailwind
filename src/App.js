5a8697660a87d7476f483ae7f2621ba3161a5e82
rttmoa <rttmoa@gmail.com>
2023-12-17 19:28:13 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import MovieList from './components/MovieList'
 function App() {
   return (
     <div>
+      <h1>新添加</h1>
+      {/*  */}
       <MovieList />
     </div>
     // <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
