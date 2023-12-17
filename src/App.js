84a5ef7f13f940fc1794027009dbb6a2051f0261
rttmoa <rttmoa@gmail.com>
2023-12-17 21:26:38 +0800

    dev add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <MovieList />
+      {/* dev 添加注释 */}
     </div>
     // <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
     //   <h1 className="text-3xl font-bold underline">
