8edb948f6ea052700ffd715fb6b491ad0c1916b4
r2 <rttmoa2@gmail.com>
2023-12-18 21:51:29 +0800

    r2 indexjs

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ import reportWebVitals from './reportWebVitals';
 
 // 我是 feature 分支添加的注释
 
+
+// 我是 r2 用户再次添加了注释
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
