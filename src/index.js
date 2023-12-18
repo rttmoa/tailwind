54c75cd94b640dabbbb369019d5c64b77677bc28
rttmoa <rttmoa@gmail.com>
2023-12-18 17:49:41 +0800

    1

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ import reportWebVitals from './reportWebVitals';
 // 我是 dev 分支添加的注释2
 // 我是 dev 分支添加的注释3
 
+// 我是 feature 分支添加的注释
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
